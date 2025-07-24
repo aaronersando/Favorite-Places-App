898edc023b0ff28ef9b615b1784f64dd97858ea3
aaronersando <agersando@gmail.com>
2025-07-24 12:30:13 +0800

    Add navigation and screens for managing favorite places
    - Implemented navigation using React Navigation.
    - Created screens: AllPlaces, AddPlace, PlaceDetails, and Map.
    - Added components: PlaceForm, PlacesList, and PlaceItem for displaying and adding places.
    - Introduced IconButton for navigation actions.
    - Updated package.json and package-lock.json for new dependencies.

---
+++ components/UI/IconButton.js
@@ -0,0 +1,28 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const IconButton = ({ icon, color, size, onPress }) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      //   android_ripple={{ color: "#ccc" }}
+    >
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
+};
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
