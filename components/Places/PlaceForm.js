073d38257981ee68017cca496bb97dd64fb6706d
aaronersando <agersando@gmail.com>
2025-07-24 12:58:24 +0800

    Implement image picker functionality and enhance UI with color constants

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../constants/colors";
+import { useState } from "react";
 
-const PlaceForm = () => {
+const PlaceForm = ({ label }) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function handleChangeTitle(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleChangeTitle}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary50: "#e3f2fd", // lightest blue
+  primary100: "#bbdefb",
+  primary200: "#90caf9",
+  primary400: "#42a5f5",
+  primary500: "#2196f3", // main blue
+  primary700: "#1976d2",
+  primary800: "#1565c0", // darkest blue
+  accent500: "#00bfae", // modern teal accent
+  gray700: "#374151", // neutral modern gray
+};
